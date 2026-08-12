7ccdf05674f5857ad9d61ddf84d2d85607d7914a
suzy75321-hub <suzy75321@gmail.com>
2026-08-13 02:29:40 +0900

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -57,7 +57,7 @@ ${recordText}
 `;
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + apiKey,
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.6-flash:generateContent?key=" + apiKey,
       {
         method: "POST",
         headers: {
